6f3a915561aca2a70fe74e18cde543e90d2b9d0b
matt <syan4@ualberta.ca>
2019-10-07 04:02:13 -0600

    integration test - take 3

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from os import environ
 import pytest
+
+from sysdm import list_unit_info
 from sysdm.__main__ import _main
 
 
@@ -20,8 +22,23 @@ def test_integration():
     test a series of operations
     """
     if not environ.get("TRAVIS", False):
-        pytest.skip("Integration test only performed on travis")
+        pytest.skip("It's recommended that integration tests only performed on travis")
 
     cmd(["create", "./tests/trivial_script_0.py", "--nolist"])
 
+    unit_info = list_unit_info()
+
+    assert unit_info == {"trivial_script_0_py": [True, True, ""]}
+
     cmd(["create", "./tests/trivial_script_1.py", "--nolist"])
+
+    unit_info = list_unit_info()
+    assert unit_info == {
+        "trivial_script_0_py": [True, True, ""],
+        "trivial_script_1_py": [True, True, ""],
+    }
+
+    cmd(["delete", "trivial_script_1_py"])
+
+    unit_info = list_unit_info()
+    assert unit_info == {"trivial_script_0_py": [True, True, ""]}
